0a82e8c4d9fe3dcbf430ab6a9a8a5dd9f9a4e938
pranaysai20 <pranaysaipadharthi@gmail.com>
2026-04-07 11:31:56 +0530

    Update print statements in main.java

--- main.java
+++ main.java
@@ -2,8 +2,9 @@ class test
 {
     public static void main(String[] args)
     {
-        System.out.println("i am test");
+        System.out.println("i am test2");
     }
 }
 class java 
-    system.out.println("i am dev")
+    system.out.println("i am dev2")
+// njefkhkadfn
